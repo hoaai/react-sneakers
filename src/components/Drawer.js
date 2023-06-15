e47c54e1c595f7b01db9caea9782566223c1a171
Arthur Mirsayapov <arturgoldaxe@mail.ru>
2023-06-15 18:53:59 +0300

    lesson 2,components

---
+++ src/components/Drawer.js
@@ -0,0 +1,58 @@
+const Drawer = () => {
+	return (
+
+		<div style={{display:"none"}} className="overlay">
+				<div className="drawer">
+					<h2 className="mb-30 d-flex justify-between ">Корзина<img className="cu-p" src="/img/btn-remove.svg" alt="Remove" /></h2>
+					
+						<div className="items">
+							<div className="cartItem d-flex align-center mb-20">
+							
+							<div style={{
+								backgroundImage:'url(/img/sneakers/1.jpg)'
+							}} className="cartItemImg">
+
+							</div>
+							<div className="mr-20px flex">
+								<p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+						</div>
+							<div className="cartItem d-flex align-center mb-20">
+								
+								<div style={{
+									backgroundImage:'url(/img/sneakers/1.jpg)'
+								}} className="cartItemImg">
+
+								</div>
+								<div className="mr-20px flex">
+									<p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+									<b>12 999 руб.</b>
+								</div>
+								<img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+							</div>
+						</div>
+							<div className="cartTotalBlock">
+							<ul>
+							<li>
+								<span>Итого:</span>
+								<div></div>
+								<b>21498 руб.</b>
+							</li>
+							<li>
+								<span>Налог 5%</span>
+								<div></div>
+								<b>1074 руб.</b>
+							</li>
+						</ul>
+						<button className="greenButton">Оформить заказ<img src="/img/arrow-right.svg" alt="arrow" /></button>
+							</div>
+						
+				</div>
+			</div>
+		
+	)
+}
+
+export default Drawer;
